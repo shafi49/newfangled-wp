deac8bdf3315b2fbb52769908a68b31e94fb7b98
SHAFI <shafiul.shafi@gmail.com>
2015-11-27 03:35:51 +0600

    contact page created
    Contact page created and Google map data embedded into it...

---
+++ page-contact.php
@@ -0,0 +1,51 @@
+<?php get_header (); ?>
+
+<?php get_header ('breadcrumb'); ?>
+
+<section id="content">
+	<div class="map">
+		<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d233614.87651890633!2d90.41251810000001!3d23.810332000000013!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1448573672216" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+		</iframe>
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+			<p>using page-contact.php</p>
+				<h4>Get in touch with us by filling <strong>contact form below</strong></h4>
+				<form id="contactform" action="contact/contact.php" method="post" class="validateform" name="send-contact">
+					<div id="sendmessage">
+						 Your message has been sent. Thank you!
+					</div>
+					<div class="row">
+						<div class="col-lg-4 field">
+							<input type="text" name="name" placeholder="* Enter your full name" data-rule="maxlen:4" data-msg="Please enter at least 4 chars">
+							<div class="validation">
+							</div>
+						</div>
+						<div class="col-lg-4 field">
+							<input type="text" name="email" placeholder="* Enter your email address" data-rule="email" data-msg="Please enter a valid email">
+							<div class="validation">
+							</div>
+						</div>
+						<div class="col-lg-4 field">
+							<input type="text" name="subject" placeholder="Enter your subject" data-rule="maxlen:4" data-msg="Please enter at least 4 chars">
+							<div class="validation">
+							</div>
+						</div>
+						<div class="col-lg-12 margintop10 field">
+							<textarea rows="12" name="message" class="input-block-level" placeholder="* Your message here..." data-rule="required" data-msg="Please write something"></textarea>
+							<div class="validation">
+							</div>
+							<p>
+								<button class="btn btn-theme margintop10 pull-left" type="submit">Submit message</button>
+								<span class="pull-right margintop20">* Please fill all required form field, thanks!</span>
+							</p>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	</section>
+
+<?php get_footer (); ?>
