3c165be23c4802c664f787a92d7d5bb71f5142c1
SHAFI <shafiul.shafi@gmail.com>
2015-11-26 22:37:52 +0600

    link added to breadcrumb menu's home

--- page-portfolio.php
+++ page-portfolio.php
@@ -10,8 +10,8 @@
 		<div class="row">
 			<div class="col-lg-12">
 				<ul class="breadcrumb">
-					<li><a href="#"><i class="fa fa-home"></i></a><i class="icon-angle-right"></i></li>
-					<li class="active">Portfolio</li>
+					<li><a href="<?php bloginfo ('url'); ?>"><i class="fa fa-home"></i></a><i class="icon-angle-right"></i></li>
+					<li class="active"><?php wp_title ('', true); ?></li>
 				</ul>
 			</div>
 		</div>
